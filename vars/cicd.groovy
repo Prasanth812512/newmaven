a5e2b377f14ab38eb8ba82b14dbab2a82519cdd3
Prasanth812512 <142538766+Prasanth812512@users.noreply.github.com>
2023-12-20 07:25:27 +0530

    some code changes

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,4 +11,13 @@ def TestDeployment(ip,contextPath)
 {
     deploy adapters: [tomcat9(credentialsId: "038ca199-1def-446a-83db-f608949d0d7a", path: "", url: "http://${ip}:8080")], contextPath: "${contextPath}", war: "**/*.war"
 }
+def RunSelenium(jobname)
+{
+
+
+    //git 'https://github.com/intelliqittrainings/FunctionalTesting.git'
+
+	sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+    
+}
 
