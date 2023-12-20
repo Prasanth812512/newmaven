4cd7fd353b43d776316b5f8c60ea86ba16de0fa7
Prasanth812512 <142538766+Prasanth812512@users.noreply.github.com>
2023-12-20 07:00:28 +0530

    change some code

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,3 +7,8 @@ def mavenBuild()
 {
     sh 'mvn package'
 }
+def TestDeployment(ip,contextPath)
+{
+    deploy adapters: [tomcat9(credentialsId: "038ca199-1def-446a-83db-f608949d0d7a", path: "", url: "http://${ip}:8080")], contextPath: "${contextPath}", war: "**/*.war"
+}
+
